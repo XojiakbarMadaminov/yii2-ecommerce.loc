77582f064b906b67a458a5c44157a6264ffe184a
xojiakbar <xmadaminov6635@gmail.com>
2022-10-11 16:22:01 +0500

    delete Products Categories

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -60,11 +60,11 @@ AppAsset::register($this);
         </li>
 
         <!-- Nav Item - Tables -->
-        <li class="nav-item">
-            <a class="nav-link" href="tables.html">
-                <i class="fas fa-fw fa-table"></i>
-                <span>Products Categories</span></a>
-        </li>
+<!--        <li class="nav-item">-->
+<!--            <a class="nav-link" href="tables.html">-->
+<!--                <i class="fas fa-fw fa-table"></i>-->
+<!--                <span>Products Categories</span></a>-->
+<!--        </li>-->
 
         <!-- Divider -->
         <hr class="sidebar-divider d-none d-md-block">
--- frontend/config/main.php
+++ frontend/config/main.php
@@ -8,6 +8,7 @@ $params = array_merge(
 
 return [
     'id' => 'app-frontend',
+    'name' => 'YII2 E-COMMERCE',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
